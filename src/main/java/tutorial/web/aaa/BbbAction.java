10ba4a603a4a2728547f81a9e8e02bdf393f685f
Yasuo Higa <higayasuo@gmail.com>
2007-11-27 12:44:47 +0000

    JSPを表示できるところまで実装しました。
    git-svn-id: https://www.seasar.org/svn/sastruts/trunk/sa-struts-tutorial@15 311f1561-e253-4dfb-a9fb-882cde7d7359

--- src/main/java/tutorial/web/aaa/BbbAction.java
+++ src/main/java/tutorial/web/aaa/BbbAction.java
@@ -1,5 +1,13 @@
 package tutorial.web.aaa;
 
+import org.seasar.struts.annotation.Execute;
+import org.seasar.struts.annotation.Result;
+
+@Result(path = "/aaa/bbb.jsp")
 public class BbbAction {
 
+	@Execute
+	public String execute() {
+		return "success";
+	}
 }
