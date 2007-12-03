5fe0546628390fceb2f0791837e61e002de60410
Yasuo Higa <higayasuo@gmail.com>
2007-12-03 10:59:53 +0000

    JSTLと組み合わせて動作するようにしました。 publicフィールドも機能します。
    git-svn-id: https://www.seasar.org/svn/sastruts/trunk/sa-struts-tutorial@22 311f1561-e253-4dfb-a9fb-882cde7d7359

--- src/main/java/tutorial/web/aaa/BbbAction.java
+++ src/main/java/tutorial/web/aaa/BbbAction.java
@@ -1,13 +1,45 @@
 package tutorial.web.aaa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.seasar.struts.annotation.Execute;
 import org.seasar.struts.annotation.Result;
 
 @Result(path = "/aaa/bbb.jsp")
 public class BbbAction {
 
+	public String name = "Seasar2";
+
+	public int index = 10;
+
+	public List<Hoge> hogeList = new ArrayList<Hoge>();
+
+	public List<List<Hoge>> hogeListList = new ArrayList<List<Hoge>>();
+
 	@Execute
 	public String execute() {
+		for (int i = 0; i < 10; i++) {
+			Hoge hoge = new Hoge();
+			hoge.aaa = "aaa" + i;
+			hoge.bbb = "bbb" + i;
+			hogeList.add(hoge);
+		}
+		for (int i = 0; i < 10; i++) {
+			List<Hoge> l = new ArrayList<Hoge>();
+			for (int j = 0; j < 2; j++) {
+				Hoge hoge = new Hoge();
+				hoge.aaa = "aaa" + i + j;
+				hoge.bbb = "bbb" + i + j;
+				l.add(hoge);
+			}
+			hogeListList.add(l);
+		}
 		return "success";
+	}
+
+	public static class Hoge {
+		public String aaa;
+		public String bbb;
 	}
 }
--- src/main/webapp/aaa/bbb.jsp
+++ src/main/webapp/aaa/bbb.jsp
@@ -1,9 +1,23 @@
+<%@page contentType="text/html charset=UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%> 
+
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Hello</title>
 </head>
 <body>
-<h1>Hello World!</h1>
+<h1><c:out value="Hello ${name}!!!"/></h1>
+<br />
+<c:forEach items="${hogeList}" var="hoge">
+${hoge.aaa} ${hoge.bbb}<br>
+</c:forEach>
+<br />
+<c:forEach items="${hogeListList}" var="hogeList">
+  <c:forEach items="${hogeList}" var="hoge">
+    ${hoge.aaa} ${hoge.bbb}
+  </c:forEach>
+  <br />
+</c:forEach>
 </body>
 </html>
