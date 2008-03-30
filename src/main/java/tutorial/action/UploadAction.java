6187ada66c06b6f89bc1592dad0c0173005d5bad
Yasuo Higa <higayasuo@gmail.com>
2008-03-30 08:58:51 +0000

    1.0.1-rc1対応。
    git-svn-id: https://www.seasar.org/svn/sastruts/trunk/sa-struts-tutorial@415 311f1561-e253-4dfb-a9fb-882cde7d7359

--- src/main/java/tutorial/action/UploadAction.java
+++ src/main/java/tutorial/action/UploadAction.java
@@ -6,14 +6,17 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
 import org.apache.struts.upload.FormFile;
 import org.seasar.framework.container.annotation.tiger.Binding;
 import org.seasar.framework.container.annotation.tiger.BindingType;
 import org.seasar.framework.exception.IORuntimeException;
 import org.seasar.struts.annotation.Execute;
 import org.seasar.struts.annotation.Required;
-import org.seasar.struts.util.MessageResourcesUtil;
+import org.seasar.struts.util.ActionMessagesUtil;
 
 public class UploadAction {
 
@@ -21,7 +24,11 @@ public class UploadAction {
 	@Binding(bindingType = BindingType.NONE)
 	public FormFile formFile;
 
-	public String message;
+	@Required
+	@Binding(bindingType = BindingType.NONE)
+	public FormFile[] formFiles;
+
+	public HttpServletRequest request;
 
 	public ServletContext application;
 
@@ -32,22 +39,31 @@ public class UploadAction {
 
 	@Execute(input = "upload.jsp")
 	public String upload() {
+		ActionMessages messages = new ActionMessages();
+		upload(formFile, messages);
+		for (FormFile file : formFiles) {
+			upload(file, messages);
+		}
+		ActionMessagesUtil.addMessages(request, messages);
+		return "upload.jsp";
+	}
+
+	protected void upload(FormFile file, ActionMessages messages) {
 		String path = application.getRealPath("/WEB-INF/work/"
-			+ formFile.getFileName());
+			+ file.getFileName());
 		try {
 			OutputStream out = new BufferedOutputStream(new FileOutputStream(
 				path));
 			try {
-				out.write(formFile.getFileData(), 0, formFile.getFileSize());
+				out.write(file.getFileData(), 0, file.getFileSize());
 			} finally {
 				out.close();
 			}
 		} catch (IOException e) {
 			throw new IORuntimeException(e);
 		}
-		message = MessageResourcesUtil.getMessageResources().getMessage(
+		messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage(
 			"messages.upload.complete",
-			path);
-		return "upload.jsp";
+			new Object[] { path }));
 	}
 }
--- webapp/upload/upload.jsp
+++ webapp/upload/upload.jsp
@@ -3,10 +3,16 @@
 <title>Upload</title>
 </head>
 <body>
-<html:errors/>
-${f:h(message)}
+<html:messages id="m" message="true">
+${f:h(m)}<br />
+</html:messages>
+
 <s:form action="/upload" enctype="multipart/form-data">
-<html:file property="formFile"/><br />
+<input type="file" name="formFile" /><br />
+<c:forEach varStatus="s" begin="0" end="1">
+<input type="file" name="formFiles[${s.index}]" /><br />
+</c:forEach>
+
 <input type="submit" name="upload" value="アップロード"/>
 </s:form>
 </body>
