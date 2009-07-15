aa67ce2403875e1dc9f265812ee2b96ca0aa161d
Yasuo Higa <higayasuo@gmail.com>
2009-07-15 02:15:31 +0000

    テストしたときの内容が間違ってコミットされていたのを修正。
    git-svn-id: https://www.seasar.org/svn/sastruts/trunk/sa-struts-tutorial@855 311f1561-e253-4dfb-a9fb-882cde7d7359

--- src/main/java/tutorial/form/AddForm.java
+++ src/main/java/tutorial/form/AddForm.java
@@ -1,14 +1,9 @@
 package tutorial.form;
 
-import java.io.Serializable;
-
 import org.seasar.struts.annotation.IntegerType;
 import org.seasar.struts.annotation.Required;
 
-//@Component(instance = InstanceType.SESSION)
-public class AddForm implements Serializable {
-
-    private static final long serialVersionUID = 1L;
+public class AddForm {
 
     @Required
     @IntegerType
